528bf72dd7f15e84b0320ada16945cbf4e23f25e
RaillyLinker <raillylinker@gmail.com>
2025-06-15 17:31:16 +0900

    update : 수정

--- nbb.py
+++ nbb.py
@@ -17,24 +17,33 @@ class RMSNorm(nn.Module):
 class UpsampleConcatBackbone(nn.Module):
     def __init__(self):
         super().__init__()
+        img_ch = 3
 
-        # todo : 처음 빼고 다 depthwise 로 해보기
         self.feature_blocks = nn.ModuleList([
-            self._make_single_conv_block(1, 40, 20, 3, 3, 0),  # 243x243 -> 81x81
-            self._make_single_conv_block(20, 80, 40, 3, 3, 0),  # 81x81 -> 27x27
-            self._make_single_conv_block(40, 160, 80, 3, 3, 0),  # 27x27 -> 9x9
-            self._make_single_conv_block(80, 320, 160, 3, 3, 0),  # 9x9 -> 3x3
-            self._make_single_conv_block(160, 640, 320, 3, 1, 0),  # 3x3 -> 1x1
+            self._make_single_conv_block(1, 512, 16, 3, 2, 1),  # 224x224 -> 112x112
+            self._make_single_conv_block(16, 512, 32, 3, 2, 1),  # 112x112 -> 56x56
+            self._make_single_conv_block(32, 512, 64, 3, 2, 1),  # 56x56 -> 28x28
+            self._make_single_conv_block(64, 512, 128, 3, 2, 1),  # 28x28 -> 14x14
+            self._make_single_conv_block(128, 512, 256, 3, 2, 1),  # 14x14 -> 7x7
         ])
+
+        self.reduce_ch_half_list = nn.ModuleList()
+        in_channels = img_ch
+        for block in self.feature_blocks:
+            out_ch = block[3].out_channels
+            in_channels = in_channels + out_ch
+            half_ch = in_channels // 2
+            self.reduce_ch_half_list.append(
+                nn.Conv2d(in_channels, half_ch, kernel_size=1, stride=1, padding=0, bias=False)
+            )
+            in_channels = half_ch
 
     def _make_single_conv_block(self, in_ch, mid_ch, out_ch, ks, strd, pdd):
         return nn.Sequential(
-            # 커널로 형태 비교
             nn.Conv2d(in_ch, mid_ch, kernel_size=ks, stride=strd, padding=pdd, bias=False),
             RMSNorm(mid_ch),
             nn.SiLU(),
 
-            # 커널 단위로 추출된 채널 벡터의 조합으로 고유값 반환(차원 밀도 높이기)
             nn.Conv2d(mid_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
             RMSNorm(out_ch),
             nn.SiLU()
@@ -43,17 +52,14 @@ class UpsampleConcatBackbone(nn.Module):
     def forward(self, x):
         target_h, target_w = x.shape[2], x.shape[3]
 
-        feat_list = [x]
+        out = x
 
         x = 0.2989 * x[:, 0:1, :, :] + 0.5870 * x[:, 1:2, :, :] + 0.1140 * x[:, 2:3, :, :]
 
         for i, block in enumerate(self.feature_blocks):
             x = block(x)
-
-            f = F.interpolate(x, size=(target_h, target_w), mode='nearest')
-            feat_list.append(f)
-
-        out = torch.cat(feat_list, dim=1)
+            out = torch.cat([out, F.interpolate(x, size=(target_h, target_w), mode='nearest')], dim=1)
+            out = self.reduce_ch_half_list[i](out)
 
         return out
 
@@ -64,13 +70,13 @@ class UpsampleConcatClassifier(nn.Module):
         super().__init__()
         self.backbone = UpsampleConcatBackbone()
 
-        dummy_input = torch.zeros(1, 3, 243, 243)
+        dummy_input = torch.zeros(1, 3, 224, 224)
         with torch.no_grad():
             backbone_output = self.backbone(dummy_input)
 
         _, backbone_output_ch, backbone_output_h, backbone_output_w = backbone_output.shape
 
-        hidden_dim = num_classes * 10
+        hidden_dim = backbone_output_ch * 2
 
         self.classifier = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
--- train.py
+++ train.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
             return None
 
         transform = transforms.Compose([
-            transforms.Resize((243, 243)),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
